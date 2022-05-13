3517066b0a9a48fd0a862e1d6f8b0ea8d5fcd58e
KTigerFIre <kwon102030@gmail.com>
2022-05-13 22:46:17 +0900

    basic menuform added

---
+++ Omok_Board_Game/Client/MenuForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Client
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.singleplaybutton = new System.Windows.Forms.Button();
+            this.exitbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // singleplaybutton
+            // 
+            this.singleplaybutton.Location = new System.Drawing.Point(330, 122);
+            this.singleplaybutton.Name = "singleplaybutton";
+            this.singleplaybutton.Size = new System.Drawing.Size(129, 48);
+            this.singleplaybutton.TabIndex = 0;
+            this.singleplaybutton.Text = "Single play";
+            this.singleplaybutton.UseVisualStyleBackColor = true;
+            this.singleplaybutton.Click += new System.EventHandler(this.singleplaybutton_Click);
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.Location = new System.Drawing.Point(330, 260);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(129, 48);
+            this.exitbutton.TabIndex = 1;
+            this.exitbutton.Text = "Exit";
+            this.exitbutton.UseVisualStyleBackColor = true;
+            this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitbutton);
+            this.Controls.Add(this.singleplaybutton);
+            this.Name = "MenuForm";
+            this.Text = "Omok";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button singleplaybutton;
+        private System.Windows.Forms.Button exitbutton;
+    }
+}
+
---
+++ Omok_Board_Game/Client/MenuForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+        /**
+         * 별도의 이벤트 처리 함수
+         */
+        void childForm_Closed(object sendor, FormClosedEventArgs e)
+        {
+            //Show();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void singleplaybutton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            SinglePlayForm singlePlayForm = new SinglePlayForm();
+            //singleplayfoirm이 닫히면 eventhandler에서 childForm_Closed 작업추가. childForm_Closed는 menu 판을 show
+            singlePlayForm.FormClosed += new FormClosedEventHandler(childForm_Closed);
+            singlePlayForm.Show();
+        }
+
+        private void exitbutton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
